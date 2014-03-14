47bfc332679652b7afaa499fedeee4d11bdf59cb
Jakub Bartecek <jbartece@redhat.com>
2014-03-14 12:13:12 +0100

    Add plugins for maven to add dependencies to jar, add logger to project

--- src/src/main/java/undertow4jenkins/Launcher.java
+++ src/src/main/java/undertow4jenkins/Launcher.java
@@ -1,28 +1,50 @@
 package undertow4jenkins;
 
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import undertow4jenkins.option.OptionParser;
+
 /**
  * @author Jakub Bartecek <jbartece@redhat.com>
  * 
  */
-public class Launcher implements Runnable {
+public class Launcher {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+    
+    @SuppressWarnings("unused")
+    private Map<String, String> options;
 
     /**
      * Field for usage, which can be overridden outside this class
      */
     public static String USAGE;
-    
+
+    public Launcher(Map<String, String> options) {
+        log.debug("constructor");
+        this.options = options;
+    }
+
     public void run() {
+        log.debug("run");
+        
     }
 
     /**
      * @param args
      */
     public static void main(String[] args) {
-        System.out.println("Undertow4Jenkins: main");
-        
-        for(String arg : args) {
-            System.out.println("Arg: " + arg);
-        }
+        Logger log = LoggerFactory.getLogger("Main");
+        log.info("Undertow4Jenkins is starting...");
+
+        OptionParser optionParser = new OptionParser();
+        Map<String, String> options = optionParser.parse(args);
+
+        Launcher launcher = new Launcher(options);
+        launcher.run();
     }
 
 }
--- src/src/main/java/undertow4jenkins/option/OptionParser.java
+++ src/src/main/java/undertow4jenkins/option/OptionParser.java
@@ -1,6 +1,32 @@
 package undertow4jenkins.option;
 
+import java.util.HashMap;
+import java.util.Map;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class OptionParser {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+    
+    public final String oHttp= "httpPort";
+    public final String oWarFile = "warfile";
+    public final String oWebRoot = "webroot";
+
+    public Map<String, String> parse(String[] args) {
+        Map<String, String> options = new HashMap<String, String>();
+        
+        for(String arg: args) {
+            if(arg.startsWith("--")) {
+//                switch(arg) {
+//                    case "oHttp":
+//                        break;
+//                }  
+            } 
+            log.info(arg);
+        }
+        return options;
+    }
 
 }
