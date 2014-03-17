8264f04fa5cad1848e8dff11e8cb3aa8c8f43a69
Jakub Bartecek <jbartece@redhat.com>
2014-03-17 17:40:30 +0100

    Rework configuration to use ResourceBundle Now it works also integrated in Jenkins

--- src/src/main/java/undertow4jenkins/util/Configuration.java
+++ src/src/main/java/undertow4jenkins/util/Configuration.java
@@ -1,7 +1,6 @@
 package undertow4jenkins.util;
 
-import java.io.IOException;
-import java.util.Properties;
+import java.util.ResourceBundle;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,32 +8,23 @@ import org.slf4j.LoggerFactory;
 public class Configuration {
 
     private static final Logger log = LoggerFactory
-            .getLogger("undertow4jenkins.configuration.Configuration");
+            .getLogger("undertow4jenkins.util.Configuration");
 
-    private static String PROPERTY_FILE_NAME = "undertow4jenkins.properties";
+    private static String PROPERTY_PATH = "undertow4jenkins.util.undertow4jenkins";
 
-    private static Properties propertiesResource = initConfiguration();
+    private static ResourceBundle propertiesResource = initConfiguration();
 
-    private static Properties initConfiguration() {
-        try {
-            Properties properties = new Properties();
-            properties.load(Thread.currentThread().getContextClassLoader()
-                    .getResourceAsStream(PROPERTY_FILE_NAME));
-            return properties;
-        } catch (IOException e) {
-            log.error("Configuration could not be initialized!", e);
-            return null;
-        }
-
+    private static ResourceBundle initConfiguration() {
+        return ResourceBundle.getBundle(PROPERTY_PATH);
     }
 
     public static String getProperty(String propertyName) {
-        return propertiesResource.getProperty(propertyName);
+        return propertiesResource.getString(propertyName).trim();
     }
 
     public static Integer getIntProperty(String propertyName) {
         try {
-            return Integer.parseInt(getProperty(propertyName));
+            return Integer.parseInt(getProperty(propertyName).trim());
         } catch (NumberFormatException e) {
             log.error("Property is not Integer!", e);
             return null;
@@ -43,13 +33,13 @@ public class Configuration {
 
     public static Boolean getBoolProperty(String propertyName) {
         try {
-            return Boolean.parseBoolean(getProperty(propertyName));
+            return Boolean.parseBoolean(getProperty(propertyName).trim());
         } catch (NumberFormatException e) {
             log.error("Property is not Boolean!", e);
             return null;
         }
     }
-    
+
     public static boolean isPropertySet(String propertyName) {
         return propertiesResource.containsKey(propertyName);
     }
