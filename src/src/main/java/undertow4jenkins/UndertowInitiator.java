671b552c22a3f12dc1a768f39e070a89f5746fe1
Jakub Bartecek <jbartece@redhat.com>
2014-04-08 16:18:39 +0200

    Create implementation of 2 types of https listener

--- src/src/main/java/undertow4jenkins/UndertowInitiator.java
+++ src/src/main/java/undertow4jenkins/UndertowInitiator.java
@@ -8,12 +8,35 @@ import io.undertow.server.handlers.resource.FileResourceManager;
 import io.undertow.servlet.api.DeploymentInfo;
 import io.undertow.servlet.api.DeploymentManager;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.security.KeyFactory;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateFactory;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.RSAPrivateKeySpec;
 
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
 import javax.servlet.ServletException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import sun.security.util.DerInputStream;
+import sun.security.util.DerValue;
+import sun.misc.BASE64Decoder;
 
 import undertow4jenkins.loader.ErrorPageLoader;
 import undertow4jenkins.loader.FilterLoader;
@@ -26,6 +49,13 @@ import undertow4jenkins.parser.WebXmlContent;
 
 public class UndertowInitiator {
 
+    private static final int MAX_PORT = 65535;
+
+    /**
+     * IP adress, which should be set to bind listener in Undertow to listen on all interfaces
+     */
+    private static final String hostAllInterfacesString = "0.0.0.0";
+
     private final Logger log = LoggerFactory.getLogger(getClass());
 
     private ClassLoader classLoader;
@@ -33,11 +63,6 @@ public class UndertowInitiator {
     private Options options;
 
     private String pathToTmpDir;
-
-    /**
-     * IP adress, which should be set to bind listener in Undertow to listen on all interfaces
-     */
-    private final String hostAllInterfacesString = "0.0.0.0";
 
     public UndertowInitiator(ClassLoader classLoader, Options options, String pathToTmpDir) {
         this.classLoader = classLoader;
@@ -75,8 +100,8 @@ public class UndertowInitiator {
             log.info("Http listener is disabled.");
         }
         else {
-            if (options.httpPort < -1) {
-                log.warn("Unallowed httpPort value. Http listener is disabled!");
+            if (options.httpPort < -1 || options.httpPort > MAX_PORT) {
+                log.warn("Unallowed httpPort value. HTTP listener is disabled!");
             }
             else {
                 if (options.httpListenAdress != null)
@@ -90,24 +115,160 @@ public class UndertowInitiator {
         }
     }
 
+    // "   --httpsKeepAliveTimeout   = how long idle HTTPS keep-alive connections are kept around (in ms; default 5000)?\n" +
+
     // "   --httpsPort              = set the https listening port. -1 to disable, Default is disabled\n" +
     // "                              if neither --httpsCertificate nor --httpsKeyStore are specified,\n" +
     // "                              https is run with one-time self-signed certificate.\n" +
     // "   --httpsListenAddress     = set the https listening address. Default is all interfaces\n" +
-    // "   --httpsDoHostnameLookups = enable host name lookups on incoming https connections (true/false). Default is false\n" +
-    // "   --httpsKeepAliveTimeout   = how long idle HTTPS keep-alive connections are kept around (in ms; default 5000)?\n" +
-    // "   --httpsKeyStore          = the location of the SSL KeyStore file.\n" +
-    // "   --httpsKeyStorePassword  = the password for the SSL KeyStore file. Default is null\n" +
+    // "   --httpsKeyManagerType    = the SSL KeyManagerFactory type (eg SunX509, IbmX509). Default is SunX509\n" +
+    private void setHttpsListener(Builder serverBuilder) {
+        if (options.httpsPort == -1)
+            return;
+
+        if (options.httpsPort < -1 || options.httpsPort > MAX_PORT) {
+            log.warn("Unallowed httpsPort value. HTTPS listener is disabled!");
+            return;
+        }
+
+        String host;
+        if (options.httpsListenAdress == null)
+            host = hostAllInterfacesString; // Listen on all interfaces
+        else
+            host = options.httpsListenAdress;
+
+        if (options.httpsKeyStore != null) {
+            createHttpsListenerWithKeyStore(serverBuilder, host, options.httpsPort);
+        }
+        else {
+            if (options.httpsCertificate != null)
+                createHttpsListenerWithCert(serverBuilder, host, options.httpsPort);
+            else
+                createHttpsListenerWithSelfSignedCert(serverBuilder, host, options.httpsPort);
+        }
+
+    }
+
+    private void createHttpsListenerWithSelfSignedCert(Builder serverBuilder, String host,
+            Integer httpsPort) {
+        // TODO Auto-generated method stub
+
+    }
+
+    // B:
     // "   --httpsCertificate       = the location of the PEM-encoded SSL certificate file.\n" +
     // "                              (the one that starts with '-----BEGIN CERTIFICATE-----')\n" +
     // "                              must be used with --httpsPrivateKey.\n" +
     // "   --httpsPrivateKey        = the location of the PEM-encoded SSL private key.\n" +
     // "                              (the one that starts with '-----BEGIN RSA PRIVATE KEY-----')\n" +
-    // "   --httpsKeyManagerType    = the SSL KeyManagerFactory type (eg SunX509, IbmX509). Default is SunX509\n" +
-    private void setHttpsListener(Builder serverBuilder) {
-        if(options.httpsPort == -1)
+    private void createHttpsListenerWithCert(Builder serverBuilder, String host,
+            Integer httpsPort) {
+        if (options.httpsPrivateKey == null) {
+            log.warn("HttpsPrivate has to be set to enable HTTPS listener with certificate. "
+                    + "HTTPS listener is disabled!");
             return;
+        }
 
+        File certFile = new File(options.httpsCertificate);
+        File privateKeyFile = new File(options.httpsPrivateKey);
+
+        try {
+            CertificateFactory certFactory = CertificateFactory.getInstance("X509");
+            Certificate cert = certFactory.generateCertificate(new FileInputStream(certFile));
+            PrivateKey privateKey = createRSAPrivateKeyFromCert(new FileReader(privateKeyFile));
+
+            char[] keyStorePassword = "changeit".toCharArray();
+
+            KeyStore keyStoreInstance = KeyStore.getInstance("JKS");
+            keyStoreInstance.load(null);
+            keyStoreInstance.setKeyEntry("hudson", privateKey, keyStorePassword,
+                    new Certificate[] { cert });
+
+            SSLContext sslContext = createSSLContext(keyStorePassword, keyStoreInstance);
+            serverBuilder.addHttpsListener(options.httpsPort, host, sslContext);
+
+        } catch (Exception e) {
+            log.warn("Failed to init SSL context. Check HTTPS options. HTTPS listener disabled!", e);
+        }
+
+    }
+
+    @SuppressWarnings("restriction")
+    private PrivateKey createRSAPrivateKeyFromCert(FileReader fileReader) throws IOException,
+            NoSuchAlgorithmException, InvalidKeySpecException {
+        ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+        BASE64Decoder decoder = new BASE64Decoder();
+
+        try {
+            BufferedReader reader = new BufferedReader(fileReader);
+            String line;
+            boolean in = false;
+
+            while ((line = reader.readLine()) != null) {
+                if (line.startsWith("-----")) {
+                    in = !in;
+                    continue;
+                }
+                if (in) {
+                    outStream.write(decoder.decodeBuffer(line));
+                }
+                // Another option to decode Base64 data: javax.xml.bind.DatatypeConverter
+                // http://stackoverflow.com/questions/469695/decode-base64-data-in-java
+            }
+        } finally {
+            fileReader.close();
+        }
+
+        DerInputStream dis = new DerInputStream(outStream.toByteArray());
+        DerValue[] seq = dis.getSequence(0);
+
+        BigInteger mod = seq[1].getBigInteger();
+        BigInteger privExpo = seq[3].getBigInteger();
+
+        KeyFactory kf = KeyFactory.getInstance("RSA");
+        return kf.generatePrivate(new RSAPrivateKeySpec(mod, privExpo));
+    }
+
+    // A:
+    // "   --httpsKeyStore          = the location of the SSL KeyStore file.\n" +
+    // "   --httpsKeyStorePassword  = the password for the SSL KeyStore file. Default is null\n" +
+    private void createHttpsListenerWithKeyStore(Builder serverBuilder, String host,
+            Integer httpsPort) {
+
+        char[] keyStorePassword;
+        if (options.httpsKeyStorePassword == null)
+            keyStorePassword = null;
+        else
+            keyStorePassword = options.httpsKeyStorePassword.toCharArray();
+
+        File keyStoreFile = new File(options.httpsKeyStore);
+        if (!keyStoreFile.exists() || !keyStoreFile.isFile()) {
+            log.warn("KeyStore not found. HTTPS listener disabled!");
+            return;
+        }
+
+        try {
+            KeyStore keyStoreInstance = KeyStore.getInstance("JKS");
+            keyStoreInstance.load(new FileInputStream(keyStoreFile), keyStorePassword);
+
+            SSLContext sslContext = createSSLContext(keyStorePassword, keyStoreInstance);
+            serverBuilder.addHttpsListener(options.httpsPort, host, sslContext);
+
+        } catch (Exception e) {
+            log.warn("Failed to init SSL context. Check HTTPS options. HTTPS listener disabled!", e);
+        }
+    }
+
+    private SSLContext createSSLContext(char[] keyStorePassword, KeyStore keyStoreInstance)
+            throws NoSuchAlgorithmException, KeyStoreException, UnrecoverableKeyException,
+            KeyManagementException {
+        KeyManagerFactory keyManagerFactory = KeyManagerFactory
+                .getInstance(options.httpsKeyManagerType);
+        keyManagerFactory.init(keyStoreInstance, keyStorePassword);
+
+        SSLContext sslContext = SSLContext.getInstance("SSL");
+        sslContext.init(keyManagerFactory.getKeyManagers(), null, null);
+        return sslContext;
     }
 
     /**
@@ -116,29 +277,29 @@ public class UndertowInitiator {
      * @param serverBuilder Prepared Undertow instance to which listener will be added
      */
     private void setAjpListener(Builder serverBuilder) {
-        if(options.ajp13Port == -1) 
+        if (options.ajp13Port == -1)
             return;
-        
-        if(options.ajp13Port < -1) {
-            log.warn("Unallowed ajpPort value. Ajp listener is disabled!");
+
+        if (options.ajp13Port < -1 || options.ajp13Port > MAX_PORT) {
+            log.warn("Unallowed ajp13Port value. AJP13 listener is disabled!");
         }
         else {
-            if(options.ajp13ListenAdress != null)
+            if (options.ajp13ListenAdress != null)
                 serverBuilder.addAjpListener(options.ajp13Port, options.ajp13ListenAdress);
             else {
-                //Listen on all interfaces
+                // Listen on all interfaces
                 serverBuilder.addAjpListener(options.ajp13Port, hostAllInterfacesString);
             }
-            
+
             log.debug("Created AJP listener");
         }
     }
 
     // "   --controlPort            = set the shutdown/control port. -1 to disable, Default disabled\n" +
     private void createControlPort(Builder serverBuilder) {
-        if(options.controlPort == -1)
+        if (options.controlPort == -1)
             return;
-        //TODO
+        // TODO
 
     }
 
@@ -147,6 +308,7 @@ public class UndertowInitiator {
         DeploymentInfo servletContainerBuilder = deployment()
                 .setClassLoader(classLoader)
                 .setContextPath("")
+                // TODO add option prefix and it here
                 .setDeploymentName(options.warfile)
                 .addListeners(ListenerLoader.createListener(webXmlContent.listeners, classLoader))
                 .addServlets(
