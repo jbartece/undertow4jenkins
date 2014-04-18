69979c02260f1fc106be762c496594d96bda3b18
Jakub Bartecek <jbartece@redhat.com>
2014-04-18 08:54:24 +0200

    Add HTTP test for all interfaces

--- src/src/test/java/undertow4jenkins/HttpConnectionTest.java
+++ src/src/test/java/undertow4jenkins/HttpConnectionTest.java
@@ -25,4 +25,18 @@ public class HttpConnectionTest extends AbstractTest {
 
         makeRequest("http://127.0.0.2:11005/CountRequestsServlet");
     }
+
+    @Test
+    public void testAllInterfaces() throws UnknownHostException, IOException, SAXException {
+        Options opts = new Options();
+        opts.warfile = "target/test-classes/test.war";
+        opts.httpPort = 11006;
+
+        containerInstance = new Launcher(opts);
+        containerInstance.run();
+
+        makeRequest("http://127.0.0.1:11006/CountRequestsServlet");
+        makeRequest("http://127.0.0.2:11006/CountRequestsServlet");
+        makeRequest("http://localhost:11006/CountRequestsServlet");
+    }
 }
