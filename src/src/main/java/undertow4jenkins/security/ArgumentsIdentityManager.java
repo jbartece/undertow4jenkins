0f8e5c07c9770f7d7f3355cace739cefab11fdc5
Jakub Bartecek <jbartece@redhat.com>
2014-04-29 11:44:11 +0200

    Create abstract supper class for identity manager

--- src/src/main/java/undertow4jenkins/security/ArgumentsIdentityManager.java
+++ src/src/main/java/undertow4jenkins/security/ArgumentsIdentityManager.java
@@ -1,24 +1,10 @@
 package undertow4jenkins.security;
 
-import io.undertow.security.idm.Account;
-import io.undertow.security.idm.Credential;
 import io.undertow.security.idm.IdentityManager;
-import io.undertow.security.idm.PasswordCredential;
 
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Map;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public class ArgumentsIdentityManager implements IdentityManager {
-
-    private final Logger log = LoggerFactory.getLogger(getClass());
-
-    private final Map<String, UserAccount> users = new HashMap<String, UserAccount>();
-
-    private final Map<String, char[]> passwords = new HashMap<String, char[]>();
+public class ArgumentsIdentityManager extends GenericIdentityManager implements IdentityManager {
 
     /**
      * Initializes identity manager and set users
@@ -38,57 +24,6 @@ public class ArgumentsIdentityManager implements IdentityManager {
             this.users.put(username, new UserAccount(username, roles));
             this.passwords.put(username, user.getValue().toCharArray());
         }
-    }
-
-    @Override
-    public Account verify(Account account) {
-        log.debug("Verify: Account: " + account.toString());
-        if (account instanceof UserAccount) {
-            return verifyCredential(account, ((UserAccount) account).getCredential());
-        }
-        else {
-            log.debug("Account is not instance of UserAccount");
-            return null;
-        }
-    }
-
-    @Override
-    public Account verify(String id, Credential credential) {
-        log.debug("Verify: Id " + id + ", " + credential.toString());
-        UserAccount account = users.get(id);
-        if (account != null) {
-            return verifyCredential(new UserAccount(account.getPrincipal(), account.getRoles(),
-                    credential), credential);
-        }
-        else
-            return null;
-    }
-
-    @Override
-    public Account verify(Credential credential) {
-        log.warn("Only authentization with username and password is supported.");
-        return null;
-    }
-
-    private Account verifyCredential(Account account, Credential credential) {
-        if (credential instanceof PasswordCredential) {
-            String username = account.getPrincipal().getName();
-            char[] password = passwords.get(username);
-            if (password == null)
-                return null; // No password for this account
-
-            char[] credentialPassword = ((PasswordCredential) credential).getPassword();
-
-            if (Arrays.equals(password, credentialPassword))
-                return account;
-            else
-                return null;
-        }
-        else {
-            log.warn("Only authentization with username and password is supported.");
-            return null;
-        }
-
     }
 
 }
---
+++ src/src/main/java/undertow4jenkins/security/GenericIdentityManager.java
@@ -0,0 +1,74 @@
+package undertow4jenkins.security;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.undertow.security.idm.Account;
+import io.undertow.security.idm.Credential;
+import io.undertow.security.idm.IdentityManager;
+import io.undertow.security.idm.PasswordCredential;
+
+public abstract class GenericIdentityManager implements IdentityManager {
+
+    protected final Logger log = LoggerFactory.getLogger(getClass());
+
+    protected final Map<String, UserAccount> users = new HashMap<String, UserAccount>();
+
+    protected final Map<String, char[]> passwords = new HashMap<String, char[]>();
+
+    @Override
+    public Account verify(Account account) {
+        log.debug("Verify: Account: " + account.toString());
+        if (account instanceof UserAccount) {
+            return verifyCredential(account, ((UserAccount) account).getCredential());
+        }
+        else {
+            log.debug("Account is not instance of UserAccount");
+            return null;
+        }
+    }
+
+    @Override
+    public Account verify(String id, Credential credential) {
+        log.debug("Verify: Id " + id + ", " + credential.toString());
+        UserAccount account = users.get(id);
+        if (account != null) {
+            return verifyCredential(new UserAccount(account.getPrincipal(), account.getRoles(),
+                    credential), credential);
+        }
+        else
+            return null;
+    }
+
+    @Override
+    public Account verify(Credential credential) {
+        log.warn("Only authentization with username and password is supported.");
+        return null;
+    }
+
+    protected Account verifyCredential(Account account, Credential credential) {
+        if (credential instanceof PasswordCredential) {
+            String username = account.getPrincipal().getName();
+            char[] password = passwords.get(username);
+            if (password == null)
+                return null; // No password for this account
+
+            char[] credentialPassword = ((PasswordCredential) credential).getPassword();
+
+            if (Arrays.equals(password, credentialPassword))
+                return account;
+            else
+                return null;
+        }
+        else {
+            log.warn("Only authentization with username and password is supported.");
+            return null;
+        }
+
+    }
+
+}
